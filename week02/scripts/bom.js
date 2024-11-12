a4bfa746a43a7ff2a34acb5b324902eb8a1d5bc2
lifegatejustice <lifegatedetom@gmail.com>
2024-11-12 13:03:44 +0100

    "Moved todo list functionality from bom.js to sandbox.js and refactored code; added new code to bom.js for creating a list item with a delete button"

--- week02/scripts/bom.js
+++ week02/scripts/bom.js
@@ -1,68 +1,10 @@
-// events.js
-let tasks = [];
+const input = document.querySelector('#favchap');
+const button = document.querySelector('button');
+const list = document.querySelector('______'); // you need to fill in the blank to reference the HTML element that is a unordered list element.
 
-function taskTemplate(task) {
-  return `
-    <li ${task.completed ? 'class="strike"' : ""}>
-      <p>${task.detail}</p>
-      <div>
-        <span data-function="delete">❎</span>
-        <span data-function="complete">✅</span>
-      </div>
-    </li>`
-}
-
-function renderTasks(tasks) {
-  // get the list element from the DOM
-  const listElement = document.querySelector("#todoList");
-  listElement.innerHTML = "";
-  // loop through the tasks array. transform (map) each task object into the appropriate HTML to represent a to-do.
-  const html = tasks.map(taskTemplate).join("");
-  listElement.innerHTML = html;
-}
-
-function newTask() {
-  // get the value entered into the #todo input
-  const task = document.querySelector("#todo").value;
-  // add it to our arrays tasks
-  tasks.push({ detail: task, completed: false });
-  // render out the list
-  renderTasks(tasks);
-}
-
-function removeTask(taskElement) {
-  // Notice how we are using taskElement instead of document as our starting point?
-  // This will restrict our search to the element instead of searching the whole document.
-  tasks = tasks.filter(
-    (task) => task.detail != taskElement.querySelector('p').innerText
-  );
-  taskElement.remove();
-}
-
-function completeTask(taskElement) {
-  const taskIndex = tasks.findIndex(
-    (task) => task.detail === taskElement.querySelector('p').innerText
-  );
-  tasks[taskIndex].completed = tasks[taskIndex].completed ? false : true;
-  taskElement.classList.toggle("strike");
-  console.log(tasks);
-}
-
-function manageTasks(e) {
-  // did they click the delete or complete icon?
-  console.log(e.target);
-  const parent = e.target.closest("li");
-  if (e.target.dataset.action === "delete") {
-    removeTask(parent);
-  }
-  if (e.target.dataset.action === "complete") {
-    completeTask(parent);
-  }
-}
-
-// Add your event listeners here
-document.querySelector("#submitTask").addEventListener("click", newTask);
-document.querySelector("#todoList").addEventListener("click", manageTasks);
-
-// render  the initial list of tasks (if any) when the page loads
-renderTasks(tasks);
+const li = document.createElement('li');
+const deleteButton = document.createElement('button');
+li.textContent = input.value;
+deleteButton.textContent = '❌';
+li.append(deleteButton);
+list.append(li);
