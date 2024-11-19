af3d6b04a7b678bbab2b43ee026ed595c9289c39
lifegatejustice <lifegatedetom@gmail.com>
2024-11-19 11:23:43 +0100

    "Replaced circle area calculation code with full name concatenation function"

--- sandbox/scripts/sandbox.js
+++ sandbox/scripts/sandbox.js
@@ -1,14 +1,5 @@
-const PI = 3.14;
-// let radius = 3;
-let area = 0;
-
-function circleArea(radius) {
-  const area = radius * radius * PI;
-  return area;
+let firstName = 'Antonia';
+let lastName = 'Francesca';
+function fullName(first, lastName) {
+  return `${first} ${lastName}`
 }
-
-area = circleArea(3);
-console.log("Area1:", area);
-// radius = 4;
-area = circleArea(4);
-console.log("Area2:", area);
